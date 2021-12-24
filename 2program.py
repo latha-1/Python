8a93ad46ba803e8f85c3e193d7b996af83d6523d
latha-1 <96233693+latha-1@users.noreply.github.com>
2021-12-24 11:13:19 +0530

    Add files via upload

---
+++ 2program.py
@@ -0,0 +1,9 @@
+class first():
+      def sub(self,num1,num2):
+          return num1 - num2 
+      def mul(self,var1,var2):
+          return var1*var2 
+          
+inst=first()
+print(inst.sub(9,3))
+print(inst.mul(var2=4,var1=5))  #keyword aregument style
